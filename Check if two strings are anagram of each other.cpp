7087548c52f1d67b7db1092c1f93f21e4200e9bc
jm5768 <147072875+jm5768@users.noreply.github.com>
2025-07-04 19:12:36 +0530

    Check if two strings are anagram of each other.cpp

--- Check if two strings are anagram of each other.cpp
+++ Check if two strings are anagram of each other.cpp
@@ -20,3 +20,18 @@ public:
         return true;
     }
 };
+//2nd Method
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        if(s.length()!=t.length()) return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]!=t[i]) return false;
+        }
+        return true;
+    }
+};
