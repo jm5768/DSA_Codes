f8923ae74e3d76cd206e54c6eb7eeda5830b41a9
jm5768 <147072875+jm5768@users.noreply.github.com>
2025-07-05 14:29:03 +0530

    Create Deletion at end (Removal of last node) in a Linked List.cpp

---
+++ Deletion at end (Removal of last node) in a Linked List.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node * next;
+
+    public:
+    Node(int data1,Node* next1)
+    {
+        data=data1;
+        next=next1;
+    }
+    Node(int data1)
+    {
+        data=data1;
+        next=nullptr;
+    }
+};
+Node* RemoveTail(Node* head)
+{
+    Node* temp= head;
+    if(temp->next==NULL||temp==NULL) return NULL;
+    while(temp->next->next!=NULL)
+    {
+        temp=temp->next;
+
+    }
+    delete temp->next;
+    temp->next=NULL;
+    return head;
+
+    
+}
+Node* ll(vector<int> arr)
+{
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++)
+    {
+        Node* temp = new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+int main ()
+{
+    int count=0;
+    vector<int> arr={2,5,8,7};
+    Node * head =ll(arr);
+    head=RemoveTail(head);
+    Node* temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<endl;
+        temp=temp->next;
+        count++;
+        
+    }
+}
